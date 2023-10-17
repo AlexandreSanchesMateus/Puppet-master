387c872f00cbbf6a3aa62180d206807a9999d440
Thoma-fr <78789215+Thoma-fr@users.noreply.github.com>
2023-10-17 11:08:56 +0200

    feedback seringue

--- Assets/Seringue_Object.cs
+++ Assets/Seringue_Object.cs
@@ -2,14 +2,16 @@ using Game;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Events;
 public class Seringue_Object : Trap
 {
+    public UnityEvent OnActivate;
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
             InflictFullDamageToPlayer();
+            OnActivate?.Invoke();
         }
     }
 }
